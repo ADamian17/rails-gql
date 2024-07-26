28e3e49603772cfe3b7061c19ff57e66b73b1c3f
ADamian17 <adonismartin90@gmail.com>
2024-07-25 21:47:34 -0700

    added types for user link

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 module Types
-  class MessageType < Types::BaseObject
+  class LinkType < Types::BaseObject
     field :id, ID, null: false
-    field :body, String
-    field :sent_by, Types::UserType, null: true, method: :user
+    field :url, String
+    field :description, String
+    field :posted_by, Types::UserType, null: true, method: :user
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module Types
-  class MessageType < Types::BaseObject
+  class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :body, String
-    field :sent_by, Types::UserType, null: true, method: :user
+    field :name, String
+    field :email, String
+    field :links, [Types::LinkType], null: false
+    field :messages, [Types::MessageType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
